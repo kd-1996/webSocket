6dbc08edb145357c9418979e15c6910d839349a2
kailash <kmoond1696@gmail.com>
2023-03-06 19:41:38 +0530

    what is STOMP?

--- src/main/java/com/example/webSocket/webSocket/config/WebSocketConfig.java
+++ src/main/java/com/example/webSocket/webSocket/config/WebSocketConfig.java
@@ -6,31 +6,33 @@ import org.springframework.web.socket.config.annotation.AbstractWebSocketMessage
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
+/**
+ * WebSocket is just a communication protocol. It doesn't know How to send a message only to users
+ * who are subscribed to a particular topic or how to send a message to a particular user. STOMP -
+ * Streaming text oriented Messaging Protocol. STOMP is simple text based protocol, it allows STOMP
+ * clients to talk with any message broker supporting the protocol. Spring provides default support
+ * for it, but we can choose any other messaging Protocol such as RabbitMQ or ActiveMQ.
+ */
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     registry.enableSimpleBroker("/topic");
     registry.setApplicationDestinationPrefixes("/app");
-    /**
-     * Broker via which will be pushing messages inside message broker.
-     */
+
+    //Broker via which will be pushing messages inside message broker.
+
   }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
 
     registry.addEndpoint("/websocket-example").withSockJS();
-    /**
-     * This is the webSocket which will be created for us. You can add multiple end points
-     * Sock-js library to create webSockets
-     * WebSocket is just a communication protocol. He doesn't know How to send a message only to users  who are subscribed to
-     * a particular topic or how to send a message to a particular user.
-     * STOMP - Streaming text oriented Messaging Protocol.
-     * STOMP is simple text based protocol, it allows STOMP clients to talk with any message broker supporting the protocol.
-     * Spring provides default support for it but we can choose any other messaging Protocol such as RabitMQ or ActiveMQ.
-     */
+    //This is the webSocket which will be created for us. You can add multiple end points Sock-js
+    // library to create webSockets
   }
 }
